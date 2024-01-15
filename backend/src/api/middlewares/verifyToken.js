a67c6b94ca370c7acf6a0bf67222af1f45b329eb
JinwooJeon1024 <richie4@kaist.ac.kr>
2024-01-15 17:40:23 +0900

    fix: Fix token error message

--- backend/src/api/middlewares/verifyToken.js
+++ backend/src/api/middlewares/verifyToken.js
@@ -2,25 +2,24 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
   const token = req.headers['authorization'];
-  if (token && token.startsWith('Bearer ')) {
+  if (!token) {
+    return res.status(403).send({ message: '토큰이 필요합니다.' });
+  }
+
+  if (token.startsWith('Bearer ')) {
     req.token = token.slice(7, token.length).trim();
   } else {
     return res.status(403).send({ message: '토큰 형식이 올바르지 않습니다.' });
   }
 
-
-  if (!token) {
-    return res.status(403).send({ message: '토큰이 필요합니다.' });
-  }
-
-try {
+  try {
     const decoded = jwt.verify(req.token, process.env.JWT_SECRET);
     req.userId = decoded.userId;
     next();
-} catch (error) {
-    return res.status(401).send({ message: `${req.token}유효하지 않은 토큰입니다.` });
-}
-
+  } catch (error) {
+    console.error(`인증 실패: 토큰 - ${req.token}, 오류 - ${error.message}`);
+    return res.status(401).send({ message: '유효하지 않은 토큰입니다.' });
+  }
 };
 
 module.exports = verifyToken;
