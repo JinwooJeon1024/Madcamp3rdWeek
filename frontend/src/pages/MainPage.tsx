913c204dfd86cc29ba98e5e9b2fa590d7da47da8
udong1 <dngus1082@gmail.com>
2024-01-16 18:03:23 +0900

    Fix : main 오류

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -5,72 +5,74 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import TextWidget from "../widgets/customWidgets/TextWidget";
 import BookmarkWidget from "../widgets/customWidgets/BookmarkWidget";
+import React from "react";
 
 function MainPage() {
   const {prevWidgets, setPrevWidgets, widgets, addWidget} = useWidgets()
   const [rightImgError, setRightImgError] = useState<boolean>(false)
   const [leftImgError, setLeftImgError] = useState<boolean>(false)
   const navigate = useNavigate();
-  let fetchedElement;
+  
+  useEffect(()=>{
+    async function fetchWidgets(){
+      try {
+        const userToken = localStorage.getItem("userToken");
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/widget`, {headers: {authorization: `Bearer ${userToken}`}});
+        console.log("response data", response.data)
 
-  async function fetchWidgets(){
-    try {
-      console.log("fetch")
-      const userToken = localStorage.getItem("userToken");
-      const response = await axios.get(`${process.env.REACT_APP_API_URL}/widget`, {headers: {authorization: `Bearer ${userToken}`}});
-      console.log(response.data)
-      response.data.map((temp: { properties: { widgetType: string; widgetTopLeftX: number; widgetTopLeftY: number; width: number; height: number; text: string; url: string; icon: string; }; _id: string; }) => 
-      {switch(temp.properties.widgetType){
-          case "TextWidget":
-            fetchedElement = (<TextWidget
+        response.data.forEach((temp: { properties: { widgetType: string; widgetTopLeftX: number; widgetTopLeftY: number; width: number; height: number; text: string; url: string; icon: string; }; _id: string; }) => 
+        { let fetchedElement;
+          switch(temp.properties.widgetType){
+            case "TextWidget":
+              // fetchedElement = React.createElement(TextWidget, {
+              //   widgetId:temp._id,
+              //   widgetType:"TextWidget",
+              //   widgetTopLeftX:temp.properties.widgetTopLeftX,
+              //   widgetTopLeftY:temp.properties.widgetTopLeftY ,
+              //   width:temp.properties.width,
+              //   height:temp.properties.height, 
+              //   text:temp.properties.text,})
+              fetchedElement = temp
+              console.log(fetchedElement)
+              addWidget(fetchedElement)
+              console.log("fetched widgets", widgets)
+              break;
+            case "BookmarkWidget":
+              fetchedElement = (<BookmarkWidget
                                 widgetId={temp._id}
-                                widgetType="TextWidget"
+                                widgetType="BookmarkWidget"
                                 widgetTopLeftX={temp.properties.widgetTopLeftX} 
                                 widgetTopLeftY={temp.properties.widgetTopLeftY} 
                                 width={temp.properties.width} 
                                 height={temp.properties.height} 
-                                text={temp.properties.text}/>)
-            console.log(fetchedElement)
-            addWidget(fetchedElement)
-            console.log(widgets)
-            break;
-          case "BookmarkWidget":
-            fetchedElement = (<BookmarkWidget
-                              widgetId={temp._id}
-                              widgetType="BookmarkWidget"
-                              widgetTopLeftX={temp.properties.widgetTopLeftX} 
-                              widgetTopLeftY={temp.properties.widgetTopLeftY} 
-                              width={temp.properties.width} 
-                              height={temp.properties.height} 
-                              url={temp.properties.url}
-                              icon={temp.properties.icon}/>)
-            addWidget(fetchedElement)
-            console.log(fetchedElement)
-            console.log(widgets)
-            break;
-          default:
-            console.log("default")
-            break;
-      }})
-      
-      console.log("widgets", widgets)
-      console.log("prevWidgets", prevWidgets)
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        const axiosError = error as AxiosError;
-        if (axiosError.response) {
-          console.error("Error submitting form : ", error.response?.data);
+                                url={temp.properties.url}
+                                icon={temp.properties.icon}/>)
+              addWidget(fetchedElement)
+              console.log(fetchedElement)
+              console.log("fetched widgets",widgets)
+              break;
+            default:
+              console.log("default")
+              break;
+        }})
+        console.log("result widgets",widgets)
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          const axiosError = error as AxiosError;
+          if (axiosError.response) {
+            console.error("Error submitting form : ", error.response?.data);
+          } else {
+            console.error("Unexpected error : ", error);
+          }
         } else {
-          console.error("Unexpected error : ", error);
+          console.error("Non-Axios error : ", error);
         }
-      } else {
-        console.error("Non-Axios error : ", error);
       }
     }
-  }
-  useEffect(()=>{
+    
     fetchWidgets()
-  }, [])
+  },[])
+
 
   function handleLogout(){
     localStorage.removeItem("userToken")
